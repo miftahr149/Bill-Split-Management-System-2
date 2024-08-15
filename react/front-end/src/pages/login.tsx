025062e31c6915f469b2f3af13a91e634c2c0a9d
miftahr149 <miftahr149@gmail.com>
2024-08-15 17:18:06 +0700

    Working on responsiveness login page

--- react/front-end/src/pages/login.tsx
+++ react/front-end/src/pages/login.tsx
@@ -1,37 +1,79 @@
-import '../assets/css/login.css'
-import favicon from '../assets/img/favicon.png'
+import "../assets/css/login.css";
+import favicon from "../assets/img/favicon.png";
+import { useState } from "react";
+
+interface LoginFieldParam {
+  name: string;
+  type: string;
+  callback: (value: string) => void;
+}
+
+const LoginField = ({ name, type, callback }: LoginFieldParam) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    callback(e.target.value);
+  };
+
+  return (
+    <div className="field d-flex flex-column">
+      <label htmlFor={name}>{name[0].toUpperCase() + name.slice(1)}</label>
+      <input
+        type={type}
+        name={name}
+        placeholder={name}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+const LoginHeader = () => {
+  return (
+    <div className="form--header d-flex">
+      <div className="header--left d-flex align-items-center justify-content-center">
+        <img src={favicon} alt="" />
+      </div>
+      <div className="header--right d-flex">
+        <h1>Bill Split Management System</h1>
+        <p>Login Page</p>
+      </div>
+    </div>
+  );
+};
+
+const LoginBody = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmitFunction = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(username);
+    console.log(password);
+  };
+
+  return (
+    <form className="form--body d-flex flex-column" onSubmit={onSubmitFunction}>
+      <LoginField name="username" type="text" callback={setUsername} />
+      <LoginField name="password" type="password" callback={setPassword} />
+      <div className="button-box d-flex justify-content-center align-items-end flex-grow-1">
+        <input
+          className="flex-grow-1 btn btn-success"
+          type="submit"
+          value="Login"
+        />
+      </div>
+    </form>
+  );
+};
 
 const Login = () => {
   return (
-    <>
-      <div className="login d-flex">
-        <div className="login--form box d-flex flex-column">
-          <div className="form--header d-flex">
-            <div className="header--left">
-              <img src={favicon} alt="" />
-            </div>
-            <div className="header--right">
-              <h1>Bill Split Management System</h1>
-              <p>Login Page</p>
-            </div>
-          </div>
-          <form className="form--body" method="POST">
-            <div className="field d-flex flex-column">
-              <label htmlFor="username">Username</label>
-              <input type="text" placeholder="username" />
-            </div>
-            <div className="field d-flex flex-column">
-              <label htmlFor="password">Password</label>
-              <input type="password" placeholder="password" />
-            </div>
-            <div className="d-flex justify-content-center align-items-center">
-              <input className="login-button btn btn-success" type="submit" value="Login" />
-            </div>
-          </form>
-        </div>
+    <div className="login d-flex justify-content-center align-items-center">
+      <div className="login--form box d-flex flex-column">
+        <LoginHeader />
+        <LoginBody />
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default Login
+export default Login;
