b36779b0431df5111be5d6a4d439974c4a1dac9d
miftahr149 <miftahr149@gmail.com>
2024-10-06 10:18:20 +0800

    adding children props to the BillSplitCard
    - destructure all the attribute in the billSplit into different component

--- react/front-end/src/components/billSplitCard.tsx
+++ react/front-end/src/components/billSplitCard.tsx
@@ -1,8 +1,9 @@
 import dollarIcon from "../assets/img/dollar.png";
+import groupIcon from "../assets/img/group.png";
 import "../assets/css/billSplitCard.css";
 import AuthContext from "../context/authContext";
-import { useContext } from "react";
 import { UserProfileContext } from "../context/userProfileContext";
+import { useContext, createContext } from "react";
 
 export interface UserParams {
   username: string;
@@ -31,16 +32,65 @@ export interface BillSplitParams {
 interface BillSplitCardParams {
   value: BillSplitParams;
   callback: (value: BillSplitParams) => void;
+  children?: JSX.Element[];
 }
 
-const BillSplitCard = ({ value, callback }: BillSplitCardParams) => {
-  const { user_amount, tag, host: hostData, name } = value;
+const BillSplitCardContext = createContext<BillSplitParams>({
+  id: 0,
+  name: "",
+  host: { username: "" },
+  description: "",
+  status: "Pending",
+  tag: [],
+  user_amount: [],
+});
+
+export const HostAttribute = () => {
+  const { getImage } = useContext(UserProfileContext);
+  const { host } = useContext(BillSplitCardContext);
+
+  return (
+    <div className="d-flex flex-center gap">
+      <img src={getImage(host.username)} className="img img--round img--xs" />
+      <p className="my-text text-bold attribute-text">{host.username}</p>
+    </div>
+  );
+};
+
+export const PriceAttribute = () => {
+  const { username } = useContext(AuthContext);
+  const { user_amount } = useContext(BillSplitCardContext);
 
   const getPrice = () => {
     const find = user_amount.find(({ user }) => user.username == username);
     if (typeof find === "undefined") return 0;
     return find.amount;
   };
+
+  const priceFormat = `RM. ${getPrice()}`;
+
+  return (
+    <div className="d-flex gap--sm flex-center">
+      <img src={dollarIcon} className="img img--round img--xs" />
+      <p className="my-text text-bold attribute-text">{priceFormat}</p>
+    </div>
+  );
+};
+
+export const AmountUserAttribute = () => {
+  const { user_amount } = useContext(BillSplitCardContext);
+  return (
+    <div className="d-flex flex-center gap">
+      <img src={groupIcon} className="img img--xs" />
+      <p className="my-text my-text--sm text-bold attribute-text">
+        {`${user_amount.length} Users Participate`}
+      </p>
+    </div>
+  );
+};
+
+const BillSplitCard = ({ value, callback, children }: BillSplitCardParams) => {
+  const { tag, name } = value;
 
   const renderTag = () => {
     return tag.map(({ name }: TagParams) => (
@@ -50,10 +100,17 @@ const BillSplitCard = ({ value, callback }: BillSplitCardParams) => {
     ));
   };
 
-  const { username } = useContext(AuthContext);
-  const { getImage } = useContext(UserProfileContext);
-  const { username: host } = hostData;
-  const priceFormat = `RM. ${getPrice()}`;
+  const renderChildren = (children: JSX.Element[]) => {
+    return children.map((value, index) => {
+      if (index === children.length - 1) return value;
+      return (
+        <>
+          {value}
+          <img src="" alt="" className="separator img img--round" />
+        </>
+      );
+    });
+  };
 
   return (
     <button
@@ -71,23 +128,14 @@ const BillSplitCard = ({ value, callback }: BillSplitCardParams) => {
         </div>
 
         <div className="d-flex align-items-center gap--l">
-          <div className="d-flex gap--sm flex-center">
-            <img
-              src={getImage(hostData.username)}
-              alt="hostImage"
-              className="img img--round img--xs"
-            />
-            <p className="my-text text-bold attribute-text">{host}</p>
-          </div>
-          <img src="" alt="" className="separator img img--round" />
-          <div className="d-flex gap--sm flex-center">
-            <img
-              src={dollarIcon}
-              alt="dollaricon"
-              className="img img--round img--xs"
-            />
-            <p className="my-text text-bold attribute-text">{priceFormat}</p>
-          </div>
+          <BillSplitCardContext.Provider value={value}>
+            {typeof children === "undefined"
+              ? renderChildren([
+                  <HostAttribute />,
+                  <PriceAttribute />,
+                ])
+              : renderChildren(children)}
+          </BillSplitCardContext.Provider>
         </div>
       </div>
     </button>
