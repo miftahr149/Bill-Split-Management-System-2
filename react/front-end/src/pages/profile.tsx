0c4a4920ef9e79ef316ab6d522b126af0e7b67b8
miftahr149 <miftahr149@gmail.com>
2025-02-25 15:23:19 +0700

    Add a fetch request from profile

--- react/front-end/src/pages/profile.tsx
+++ react/front-end/src/pages/profile.tsx
@@ -2,17 +2,44 @@ import "../assets/css/profile.css";
 import Navbar from "../components/navbar";
 import AuthContext from "../context/authContext";
 import { UserProfileContext } from "../context/userProfileContext";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { APIFetch, setBackendURL, tryCatchFetch } from "../utility/myapi";
+
+interface BankData {
+  name: string;
+  number: string;
+  qr: string;
+  owner: number;
+};
+
+interface TelegramInfo {
+  telegram_usename: string
+}
+
+interface UserProfileInfo {
+  bank_info: BankData[];
+  email: string;
+  telegram_info: TelegramInfo;
+}
+
+const setDefaultProfileInfo: (() => UserProfileInfo) = () => ({
+  bank_info: [],
+  email: "",
+  telegram_info: {
+    telegram_usename: ""
+  }
+})
 
 const Profile = () => {
   const { getImage } = useContext(UserProfileContext);
   const { username } = useContext(AuthContext);
 
-  const testAPI = () => {
+  const [{email}, setProfileInfo] = useState<UserProfileInfo>(setDefaultProfileInfo);
+
+  const getProfileInfo = () => {
     tryCatchFetch(async () => {
-      const data = await APIFetch({
+      const data: UserProfileInfo = await APIFetch({
         URL: setBackendURL("getUserProfileInfo"),
         method: "POST",
         headers: {
@@ -20,13 +47,11 @@ const Profile = () => {
         },
         body: JSON.stringify({username: username})
       });
-      console.log(data);
+      setProfileInfo(() => data);
     })
   }
 
-  useEffect(() => {
-    testAPI();
-  }, [])
+  useEffect(getProfileInfo, []);
 
   return (
     <div className="pages d-flex flex-column">
@@ -47,8 +72,10 @@ const Profile = () => {
         </div>
         <div className="info px-5">
           <p className="fs-1 info-header my-text my-text-bold">Email</p>
-          <div className="info-block d-flex align-items-center px-3 py-2">
+          <div className="info-block d-flex align-items-center px-3 py-2 gap-5">
             <i className="bi bi-envelope-fill fs-1" />
+            <p className="my-text fs-2 flex-grow-1">{email}</p>
+            <button className="btn btn-success">Change Email</button>
           </div>
         </div>
       </main>
