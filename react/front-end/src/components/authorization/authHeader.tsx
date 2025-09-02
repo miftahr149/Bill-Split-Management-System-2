bd872b7d066dac4455665fe32e50b8802490a44c
miftahr149 <miftahr149@gmail.com>
2025-09-02 09:33:24 +0700

    Change the layout of authHeader component

--- react/front-end/src/components/authorization/authHeader.tsx
+++ react/front-end/src/components/authorization/authHeader.tsx
@@ -1,24 +1,17 @@
-import favicon from "../../assets/img/favicon.png"
+import favicon from "../../assets/img/favicon.png";
 
 interface AuthHeaderParams {
   title: string;
 }
 
-/** Display header for the authentication pages 
+/** Display header for the authentication pages
  *  @param {AuthHeaderParams} title - the title of this pages
  */
 const AuthHeader = ({ title }: AuthHeaderParams) => {
   return (
-    <div className="header-box d-flex box">
-      <div className="flex-grow-1 d-flex align-items-center justify-content-center">
-        <img src={favicon} alt="" className="img img--round" />
-      </div>
-      <div className="text-box d-flex justify-content-center">
-        <h1 className="my-header my-header--sm">
-          Bill Split Management System
-        </h1>
-        <p className="my-text">{title}</p>
-      </div>
+    <div className="auth-header box">
+      <h1 className="name">Bill-Split Management System</h1>
+      <p className="page-name">{title}</p>
     </div>
   );
 };
